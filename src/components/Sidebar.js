28d9f2b11ebaaecc47d308b025aa5a5e3a851262
subodhrathour17 <y*********@gmail.com>
2025-04-29 23:06:03 +0530

    first commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/Sidebar.css";
+import { FaBars } from "react-icons/fa";
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => setIsOpen(!isOpen);
+
   return (
-    <nav className='sidebar'>
-      <ul>
-        <li>Dashboard</li>
-        <li className='active'>Users</li>
-        <li>Settings</li>
-      </ul>
-    </nav>
+    <div className='sidebar-container'>
+      <div className='hamburger' onClick={toggleSidebar}>
+        <FaBars />
+      </div>
+
+      <div className={`sidebar ${isOpen ? "open" : ""}`}>
+        <ul>
+          <li>Dashboard</li>
+          <li className='active'>Users</li> {/* Users is active */}
+          <li>Settings</li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
